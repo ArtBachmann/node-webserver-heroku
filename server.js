f96161f8ecf5ab21b6f328836b5a66eea00d8382
art <bachmann.art@gmail.com>
2018-10-08 10:07:34 +0300

    project page added

--- server.js
+++ server.js
@@ -12,54 +12,57 @@ const app = express()
 // with its methods to respond aka send back.
 
 hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
-  const now = new Date().toString()
-  const log = `${now}: ${req.method} ${req.url}`
-  console.log(log)
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {
-      console.log('Unable to append to server.log')
-    }
-  })
-  next()
-})
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs')
-})
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
 
-app.use(express.static(__dirname + '/public'))
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear()
-})
+  return new Date().getFullYear();
+});
 
 hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase()
-})
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
-  //res.send('<h1>Hello Express!</h1>')
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website'
-  })
-})
+  });
+});
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page From the Server'
-  })
-})
+    pageTitle: 'About Page'
+  });
+});
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
-    errorMessage: 'Something went wrong!!!'
-  })
-})
+    errorMessage: 'Unable to handle request'
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}`)
-})
+  console.log(`Server is up on port ${port}`);
+});
